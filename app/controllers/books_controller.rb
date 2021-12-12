986fefd286550ebb175fd22fe289354a2e55118a
EC2 Default User <ec2-user@ip-172-31-9-19.ec2.internal>
2021-12-12 12:32:26 +0000

    [Finish]3-9

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,11 +3,50 @@ class BooksController < ApplicationController
   end
 
   def index
+    @books = Book.all
+    @book = Book.new
   end
 
   def show
+    @book = Book.find(params[:id])
+  end
+  
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id)
+      flash[:notice] = "Book was successfully created."
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def edit
+    @book = Book.find(params[:id])
+    flash[:notice] = "Book was successfully updated."
   end
+  
+  def update
+    @book = Book.find(params[:id])
+    # 更新前のデータ.update(更新したいカラム: 更新後の値)
+    if @book.update(book_params)
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+    flash[:notice] = "Book was successfully destroyed."
+  end
+  
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+  
 end
